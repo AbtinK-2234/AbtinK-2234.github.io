daa7915ce7eb42331846e9d474cc7f37c94d0458
Abtin K <akhashayar26@berkeley.edu>
2023-04-27 00:27:02 -0700

    finished final!

---
+++ final/assets/script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById("Home");
+function changeColor() { 
+    document.getElementById("background").style.backgroundColor = "purple";
+    document.getElementById("journey").style.color = "black";
+    
+   
+    
+}
+
+button.onclick = changeColor;
+
+let img1 = document.getElementById("imgflush");
